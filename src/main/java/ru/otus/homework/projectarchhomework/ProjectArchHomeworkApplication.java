9f48c75ef609cbf479a8b8a9fead490ccdbc5545
tsolomatina <Hfgbl123>
2020-02-20 17:02:20 +0300

    Initial commit

---
+++ src/main/java/ru/otus/homework/projectarchhomework/ProjectArchHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.homework.projectarchhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectArchHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectArchHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/homework/projectarchhomework/ProjectArchHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homework.projectarchhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectArchHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
