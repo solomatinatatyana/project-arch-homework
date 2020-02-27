4b2f3e2938cc1e4354cfc22ec0171a0bd15c1107
tsolomatina <Hfgbl123>
2020-02-27 15:57:08 +0300

    CompanyTest fix

--- src/main/java/ru/otus/homework/projectarchhomework/pagesandblocks/pages/CompanyPage.java
+++ src/main/java/ru/otus/homework/projectarchhomework/pagesandblocks/pages/CompanyPage.java
@@ -2,6 +2,7 @@ package ru.otus.homework.projectarchhomework.pagesandblocks.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,12 +24,42 @@ public class CompanyPage extends AbstractPage{
     @FindBy(id = "companies")
     public List<WebElement> companiesList;
 
+    @FindBy(css = ".table-grid__item.table-grid__item_right")
+    public WebElement companyRightGrid;
+
+    @FindBy(css = "button[data-state='follow']")
+    public WebElement subscribeButton;
+
+    @FindBy(css = "button[data-state='unfollow']")
+    public WebElement unsubscribeButton;
+
 
     public void searchCompany(String company){
         WebDriverWait wait = new WebDriverWait(driver,50L);
         wait.until(ExpectedConditions.visibilityOfAllElements(this.searchCompanyTextInput));
         searchCompanyTextInput.clear();
         searchCompanyTextInput.sendKeys(company);
+    }
+
+    /**
+     * Подписаться/отписаться
+     */
+    public void subscribe(String actions){
+        Actions action = new Actions(driver);
+        action.moveToElement(companyRightGrid).build().perform();
+        switch (actions){
+            case "follow":
+                WebDriverWait wait = new WebDriverWait(driver,50L);
+                wait.until(ExpectedConditions.visibilityOfAllElements(this.subscribeButton));
+                action.moveToElement(subscribeButton).click().build().perform();
+                break;
+            case "unfollow":
+                WebDriverWait wait2 = new WebDriverWait(driver,50L);
+                wait2.until(ExpectedConditions.visibilityOfAllElements(this.unsubscribeButton));
+                action.moveToElement(unsubscribeButton).click().build().perform();
+                break;
+
+        }
     }
 
 }
--- src/main/java/ru/otus/homework/projectarchhomework/tests/CompanyTest.java
+++ src/main/java/ru/otus/homework/projectarchhomework/tests/CompanyTest.java
@@ -2,6 +2,10 @@ package ru.otus.homework.projectarchhomework.tests;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -21,6 +25,7 @@ import ru.otus.homework.projectarchhomework.services.auth.AuthorizationService;
 @Test(groups = "smoke")
 public class CompanyTest extends BaseWebDrivingTest {
     private Logger log = LogManager.getLogger(MainPageTest.class);
+    private Actions actions;
     private SoftAssert softAssert = new SoftAssert();
     @Autowired
     private AuthorizationService authorizationService;
@@ -33,13 +38,48 @@ public class CompanyTest extends BaseWebDrivingTest {
 
     @BeforeClass(alwaysRun = true)
     public void init(){
-        authorizationService.open(config.getUrl());
+        actions = new Actions(driver);
+        authorizationService.doLogin(config.getUrl(),config.getUsername(),config.getPassword());
         mainPage.goToNavPage(mainPage.mainPageTabsBlock.companyButton);
     }
 
     @Test(description = "Найти компанию - Сбербанк. Проверить, что компания найдена")
     public void searchCompany(){
+        log.info("Ищем кампанию: [{}]", testCompany);
         companyPage.searchCompany(testCompany);
-        Assert.assertEquals(companyPage.companiesList.size(),1,"Кампания не найдена");
+        Assert.assertEquals(companyPage.companiesList.size(),1,"Кампания ["+ testCompany +"] не найдена");
+    }
+
+    @Test(description = "Нажать кнопку 'Подписаться'. " +
+            "Проверить, что при наведении кнопка меняет цвет на зеленый" +
+            "Проверить, что после подписки отображается 'Подписан'",
+        dependsOnMethods = "searchCompany")
+    public void checkSubscription(){
+        companyPage.subscribe("follow");
+        log.info("Подписка на компанию [{}] оформлена", testCompany);
+        actions.moveToElement(companyPage.companyRightGrid).build().perform();
+        WebDriverWait wait = new WebDriverWait(driver,50L);
+        wait.until(ExpectedConditions.visibilityOfAllElements(companyPage.unsubscribeButton));
+        String text = companyPage.unsubscribeButton.getText();
+        softAssert.assertEquals(text, "Подписан", "Неверное название кнопки");
+        softAssert.assertEquals(Color.fromString(companyPage.unsubscribeButton.getCssValue("background-color")).asHex(),"#689d29",
+                "Неверный цвет кнопки - не зеленый");
+        softAssert.assertAll();
+    }
+
+    @Test(description = "Нажать кнопку 'Отписаться'. Проверить, что после отписки отображается 'Подписаться'",
+        dependsOnMethods = "checkSubscription", alwaysRun = true)
+    public void checkUnSubscription(){
+        companyPage.subscribe("unfollow");
+        log.info("Подписка на компанию [{}] отменена", testCompany);
+        actions.moveToElement(companyPage.companyRightGrid).build().perform();
+        WebDriverWait wait = new WebDriverWait(driver,50L);
+        wait.until(ExpectedConditions.visibilityOfAllElements(companyPage.subscribeButton));
+        String text = companyPage.subscribeButton.getText();
+        softAssert.assertEquals(text, "Подписаться", "Неверное название кнопки");
+        System.out.println(Color.fromString(companyPage.subscribeButton.getCssValue("background-color")).asHex());
+        softAssert.assertEquals(Color.fromString(companyPage.subscribeButton.getCssValue("background-color")).asHex(),"#000000",
+                "Неверный цвет кнопки - не белый");
+        softAssert.assertAll();
     }
 }
