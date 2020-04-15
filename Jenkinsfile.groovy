a08fc00d36d23ddfb1feac1d09858a5cdf9afc7c
Tatiana <tokio9507@mail.ru>
2020-04-15 17:29:55 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,31 +2,33 @@ node {
 
     RECIPIENT = "tokio9507@gmail.com"
 
-    stage('Run Tests') {
-        parallel(
-                runTests:{build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')]}
-                //runTests: { build 'Test' }
-        )
+    try {
+        stage('Run Tests') {
+            parallel(
+                    runTests: { build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')] }
+                    //runTests: { build 'Test' }
+            )
+        }
+    }catch(ex){
+
+    }finally{
+        echo "Sending e-mail"
+        BUILD_DURATION = "${currentBuild.durationString.replace(' and counting', '')}"
+
+        emailext body: "Автотесты OTUS, <br>Длительность прогона: " + BUILD_DURATION,
+                mimeType: 'text/html',
+                subject: "Автотесты OTUS, браузер ${BROWSER}",
+                to: "${RECIPIENT}",
+                replyTo: "${RECIPIENT}"
     }
     //stage ('Generate Smoke report'){
 
     //}
 
-    stage ('SendMailReport'){
-        echo "Sending e-mail"
-        BUILD_DURATION = "${currentBuild.durationString.replace(' and counting', '')}"
+    //stage ('SendMailReport'){
 
-        emailext body: "Автотесты OTUS, <br>Длительность прогона: " + BUILD_DURATION,
 
-                mimeType: 'text/html',
-
-                subject: "Автотесты OTUS, браузер ${BROWSER}",
-
-                to: "${RECIPIENT}",
-
-                replyTo: "${RECIPIENT}"
-
-    }
+    //}
 
 
 
