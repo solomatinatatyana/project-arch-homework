ff29cec2bf4b95d4681040b80e4b4377a1660d70
Tatiana <tokio9507@mail.ru>
2020-04-15 17:07:43 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,8 +4,8 @@ node {
 
     stage('Run Tests') {
         parallel(
-                //runTests:{build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')]}
-                runTests: { build 'Test' }
+                runTests:{build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')]}
+                //runTests: { build 'Test' }
         )
     }
 }
