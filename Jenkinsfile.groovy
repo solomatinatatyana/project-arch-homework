d20ddcdd03012bbf3929f113a1b6ebc6dae1fa7f
Tatiana <tokio9507@mail.ru>
2020-04-16 17:29:11 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,7 +10,7 @@ node {
             )*/
             checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
             bat 'echo Run'
-            bat label: '', script: 'C:\\Users\\Tatiana\\maven\\apache-maven-3.6.3\\bin\\mvn clean test'
+            bat label: '', script: 'C:/Users/Tatiana/maven/apache-maven-3.6.3/bin/mvn clean test'
             //bat "mvn clean test -Dbrowser="+BROWSER
         }
     }catch(ex){
