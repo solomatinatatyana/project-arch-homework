e5c2189d01bd181d6805258a3f39fceb520839f2
Tatiana <tokio9507@mail.ru>
2020-04-16 16:43:24 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,8 +9,8 @@ node {
                     //runTests: { build 'Test' }
             )*/
             def info = checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
-            bat 'echo %PATH%'
-            //sh "mvn clean test -Dbrowser="+BROWSER
+            bat 'echo Run'
+            bat "mvn clean test -Dbrowser="+BROWSER
         }
     }catch(ex){
         currentBuild.result = 'FAILURE'
