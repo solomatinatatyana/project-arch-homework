62b3db0e6f970a974ec60628991a33a2c72b1a4a
Tatiana <tokio9507@mail.ru>
2020-04-16 17:28:08 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,9 +8,9 @@ node {
                     runTests: { build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')] }
                     //runTests: { build 'Test' }
             )*/
-            def info = checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
             bat 'echo Run'
-            bat label: '', script: 'mvn clean test'
+            bat label: '', script: 'C:\\Users\\Tatiana\\maven\\apache-maven-3.6.3\\bin\\mvn clean test'
             //bat "mvn clean test -Dbrowser="+BROWSER
         }
     }catch(ex){
