9bb6bb17ef35e7346b423121e32dd3eee8456113
Tatiana <tokio9507@mail.ru>
2020-04-16 18:12:35 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,14 +4,9 @@ node {
 
     try {
         stage('Run Tests') {
-            /*parallel(
-                    runTests: { build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')] }
-                    //runTests: { build 'Test' }
-            )*/
             checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
             bat 'echo Run'
             bat label: '', script: 'C:/Users/Tatiana/maven/apache-maven-3.6.3/bin/mvn clean test -Dbrowser='+BROWSER
-            //bat "mvn clean test -Dbrowser="+BROWSER
         }
     }catch(ex){
         currentBuild.result = 'FAILURE'
