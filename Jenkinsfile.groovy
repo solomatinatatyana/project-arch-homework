ddafbcf6a09493b3afffb9a72b6d66098a046391
Tatiana <tokio9507@mail.ru>
2020-04-16 16:02:12 +0300

    run Job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,6 +8,7 @@ node {
                     runTests: { build job: 'PipeJob', parameters: [string(name: 'browser', value: 'CHROME'), gitParameter(name: 'BRANCH', value: 'origin/master')] }
                     //runTests: { build 'Test' }
             )*/
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
             sh "mvn clean test -Dbrowser="+BROWSER
         }
     }catch(ex){
