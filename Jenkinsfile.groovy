0896a953160e45aa15a254020ee41b8fc1c4fda3
Tatiana <tokio9507@mail.ru>
2020-04-17 16:42:26 +0300

    add Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,6 @@ import jenkins.*
 import jenkins.model.*
 import hudson.*
 import hudson.model.*
-properties([pipelineTriggers([githubPush()])])
 node {
 
     RECIPIENT = "tokio9507@gmail.com"
@@ -12,7 +11,8 @@ node {
 
     try {
         stage('Run Tests') {
-            //checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
+            properties([pipelineTriggers([githubPush()])])
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
             bat 'echo Run'
             bat label: '', script: MAVEN_HOME + 'mvn clean test -Dbrowser='+BROWSER
             //runTests:{build 'Test'}
