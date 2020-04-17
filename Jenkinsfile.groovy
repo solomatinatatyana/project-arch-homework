80650187402e6f3997b41e9b69b63cc6be1802d1
Tatiana <tokio9507@mail.ru>
2020-04-17 10:47:09 +0300

    fix CompanyTest

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -98,14 +98,12 @@ def notifySlack(String buildStatus = 'STARTED') {
     } else {
         color = '#FF9FA1'
     }
-    def msg = '''${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}<br>
-    Branch: $BRANCH <br>
-    Browser: $browser<br>
-    Total:
-    Success: <br>
-    Fail: <br>
-    Skipped: <br>
-   
-    '''
+    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}<br>"
+    "Branch: $BRANCH <br>"
+    "Browser: $browser<br>"
+    "Total:"
+    "Success: <br>"
+    "Fail: <br>"
+    "Skipped: <br>"
     slackSend(color: color, message: msg)
 }
