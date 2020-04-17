e3e6e706b9b9507ddd66de19a4b9ede97699a270
Tatiana <tokio9507@mail.ru>
2020-04-17 10:24:22 +0300

    fix CompanyTest

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -96,6 +96,52 @@ def notifySlack(String buildStatus = 'STARTED') {
     } else {
         color = '#FF9FA1'
     }
-    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+    def msg = '''${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}<br>
+    <!DOCTYPE html>
+<html>
+<body>
+<table style="width=100%" border=1>
+                <tr>
+                               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Build Number</font></b></td>
+                               <td>$BUILD_NUMBER</td>
+                </tr>
+    <tr>
+                               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Branch</td>
+                               <td>$BRANCH</td>
+                </tr>
+    <tr>
+                               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Browser</td>
+                               <td>$browser</td>
+                </tr>
+    <tr>
+                               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Status</td>
+                               <td>$BUILD_STATUS</td>
+                </tr>
+                               <td BGCOLOR="#0099CC" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Total tests count</font></b></td>
+                               <td BGCOLOR="#FFFFFF"><FONT COLOR=#0099CC FACE="Geneva, Arial" SIZE=2><b>${TEST_COUNTS,var="total"}</font></b></td>      
+                </tr>
+                <tr>
+                               <td BGCOLOR="#04B431" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Success</font></b></td>
+        <td BGCOLOR="#FFFFFF"><FONT COLOR=#04B431 FACE="Geneva, Arial" SIZE=2><b>${TEST_COUNTS,var="pass"}</font></b></td>       
+                </tr>
+                <tr>
+                               <td BGCOLOR="#DF3A01" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Unstable tests</b></font></td>
+                               <td BGCOLOR="#FFFFFF"><FONT COLOR=#DF3A01 FACE="Geneva, Arial" SIZE=2><b>${TEST_COUNTS,var="fail"}</font></b></td>       
+                </tr>   
+                <tr>
+               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Skipped tests</font></b></td>
+               <td BGCOLOR="#FFFFFF"><FONT COLOR=#A4A4A4 FACE="Geneva, Arial" SIZE=2><b>${TEST_COUNTS,var="skip"}</font></b></td>       
+        </tr>
+        <tr>
+                               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Duration</b></td>
+                               <td>'''+ BUILD_DURATION +'''</td>
+                </tr>
+                <tr>
+                               <td BGCOLOR="#A4A4A4" width="20%"><FONT COLOR=white FACE="Geneva, Arial" SIZE=2><b>Link to report</b></td>
+                               <td>${BUILD_URL}allure</td>
+                </tr>  
+</table>
+</html>
+</body>'''
     slackSend(color: color, message: msg)
 }
