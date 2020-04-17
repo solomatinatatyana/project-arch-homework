27e7f8e58c151dab1f13d391bb0e8ff5d4599be6
Tatiana <tokio9507@mail.ru>
2020-04-17 10:25:36 +0300

    fix CompanyTest

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,8 @@ node {
         stage('Run Tests') {
             //checkout([$class: 'GitSCM', branches: [[name: '*/master']],  userRemoteConfigs: [[credentialsId: '62b53291-36d6-4ccb-95cf-efa68b08f788', url: 'https://github.com/solomatinatatyana/project-arch-homework']]])
             bat 'echo Run'
-            bat label: '', script: MAVEN_HOME + 'mvn clean test -Dbrowser='+BROWSER
+            //bat label: '', script: MAVEN_HOME + 'mvn clean test -Dbrowser='+BROWSER
+            runTests:{build 'Test'}
         }
     }catch(ex){
         currentBuild.result = 'FAILURE'
